cff3c06cb890639e49efb73c8658dcb2312a937c
Renan Santana Desiderio <renan_ota@hotmail.com>
2018-02-16 01:04:21 -0200

    Melhorias no codigo

--- change_db_acruxx.py
+++ change_db_acruxx.py
@@ -13,7 +13,7 @@ def saveConfig():
 		fileSave.write(pathV2 + "\n")
 		fileSave.close()
 
-		return {"V1" : pathV1, "V2" : pathV2}
+		return [pathV1, pathV2]
 
 	else:
 		print("Informe os caminhos de ambos os sistemas.")
@@ -27,58 +27,36 @@ def loadConfig():
 	pathV2 = file.readline().rstrip() # rstrip para tirar o "\n" do final da string
 	file.close()
 
-	return {"V1" : pathV1, "V2" : pathV2}
+	return [pathV1, pathV2]
 
-def replace(paths):
-	# Função que faz a substituição das linhas nos arquivos.
+def replace(paths, banco):
 
-	banco = input("Digite o nome do banco que deseja alterar:\ngpd_")
-
-	# Carregar os config.ini do V1 e V2
-	configV1 = open(paths["V1"], "r", encoding="ISO8859-1")
-	configV2 = open(paths["V2"], "r", encoding="ISO8859-1")
+	conteudos = []
+	novosConfigs  =[]
 	
-	# Ler os config.ini
-	conteudoV1 = configV1.readlines()
-	conteudoV2 = configV2.readlines()
+	# Carregar os config.ini do V1 e V2
+	for file in paths:
+		config = open(file, "r", encoding="ISO8859-1")
+		conteudos.append(config.readlines())
+		config.close()
+		# Abrir novamente os configs para serem sobreescritos
+		novosConfigs.append(open(file, "w", encoding="ISO8859-1"))
 
-	# Fechar os arquivos
-	configV1.close()
-	configV2.close()
+	indice = 0
 
-	# Abrir novamente os configs, sobreescrevendo-os
-	novoConfigV1 = open(paths["V1"], "w", encoding="ISO8859-1")
-	novoConfigV2 = open(paths["V2"], "w", encoding="ISO8859-1")
-
-	# Novos conteudos de ambos os configs
-	novoConteudoV1 = []
-	novoConteudoV2 = []
-
-	# Procurar e fazer a substituicao no V1
-	for linha in conteudoV1:
-		# print(linha, end="") # end="" faz com que nao pule uma linha no print
-		resultado = re.match("^db=g", linha)
-		if resultado:
-			novoConteudoV1.append("db=gpd_{};\n".format(banco))
-		else:
-			novoConteudoV1.append(linha)
-
-	# Procurar e fazer a substituicao no V2
-	for linha in conteudoV2:
-		# print(linha, end="") # end="" faz com que nao pule uma linha no print
-		resultado = re.match("^db=g", linha)
-		if resultado:
-			novoConteudoV2.append("db=gpd_{};\n".format(banco))
-		else:
-			novoConteudoV2.append(linha)
-
-	# Escreve o novo conteudo gerado nos config.ini abertos
-	novoConfigV1.writelines(novoConteudoV1)
-	novoConfigV2.writelines(novoConteudoV2)
-
-	# Fecha os arquivos
-	novoConfigV1.close()
-	novoConfigV2.close()
+	for conteudo in conteudos:
+		novoConteudo = []
+		for linha in conteudo:
+			resultado = re.match("^db=g", linha)
+			if resultado:
+				novoConteudo.append("db=gpd_{};\n".format(banco))
+			else:
+				novoConteudo.append(linha)
+		# Escreve o novo conteudo gerado nos config.ini abertos
+		novosConfigs[indice].writelines(novoConteudo)
+		# Fecha os arquivos
+		novosConfigs[indice].close()
+		indice += 1
 
 def main():
 
@@ -86,8 +64,10 @@ def main():
 	# Criar o config caso esse não exista ainda, ou ler ele caso ja exista
 	paths = loadConfig() if os.path.isfile("config.txt") else saveConfig()
 
+	banco = input("Digite o nome do banco que deseja alterar:\ngpd_")
+
 	# Fazer a substituicao
-	replace(paths)
+	replace(paths, banco)
 
 # Roda a porra toda
 if __name__ == '__main__':
